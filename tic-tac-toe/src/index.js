39db533b7091473ac12250740269aabacf9a50de
IFE2 <2483690020@qq.com>
2018-08-17 11:38:39 +0800

    lifting state up

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -12,47 +12,18 @@ function Square(props) {
 }
 // 类继承方式
 class Board extends React.Component {
-  constructor() {
-    // 必须调用 super()方法才能在继承父类的子类中正确获取到类型的 this
-    super()
-    // 每个组件的 state 都是它私有的。
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
-  handleClick(i) {
-    const squares = this.state.squares.slice()
-    if (calculateWinner(squares) || squares[i]) {
-      return
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O'
-    // 采用替换的方式而不是直接修改原数组
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    })
-  }
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares)
-    let status
-    if (winner) {
-      status = 'Winner: ' + winner
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-    }
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -74,14 +45,54 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor() {
+    // 必须调用 super()方法才能在继承父类的子类中正确获取到类型的 this
+    super()
+    // 每个组件的 state 都是它私有的
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null)
+      }],
+      xIsNext: true,
+    }
+  }
+  handleClick(i) {
+    const history = this.state.history
+    const current = history[history.length - 1]
+    const squares = current.squares.slice()
+    if (calculateWinner(squares) || squares[i]) {
+      return
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    // 采用替换的方式而不是直接修改原数组
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    })
+  }
   render() {
+    const history = this.state.history
+    const current = history[history.length - 1]
+    const winner = calculateWinner(current.squares)
+
+    let status
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
