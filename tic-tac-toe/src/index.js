ff3a346b572e71a345c52a4f00506488a2f5e50f
IFE2 <2483690020@qq.com>
2018-08-17 16:35:51 +0800

    optimize

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -91,7 +91,7 @@ class Game extends React.Component {
         'Game start'
       return (
         <li key={move}>
-          <a onClick={() => this.jumpTo(move)}>{desc}</a>
+          <a href="javascript:void(0)" onClick={() => this.jumpTo(move)}>{desc}</a>
         </li>
       )
     })
