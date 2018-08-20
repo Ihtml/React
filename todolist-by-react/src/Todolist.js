88891059a8aad6a0243847a40bd7adb6d789c36b
IFE2 <2483690020@qq.com>
2018-08-20 14:59:26 +0800

    add feature

---
+++ todolist-by-react/src/Todolist.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class Todolist extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [],
+      inputValue: ''
+    }
+  }
+  handleBtnClick() {
+    console.log(this) //undefined
+    this.setState({
+      list: [...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)} />
+          <button onClick={this.handleBtnClick.bind(this)}>add</button>
+        </div>
+        <ul>
+          {
+            this.state.list.map((item, index) => {
+              return <li key={index}>{item}</li>
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Todolist;
--- todolist-by-react/src/index.js
+++ todolist-by-react/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+// 把组件挂载到DOM上
 import ReactDOM from 'react-dom';
-import App from './App';
+// App组件，大写字母开头
+import Todolist from './Todolist';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Todolist />, document.getElementById('root'));
