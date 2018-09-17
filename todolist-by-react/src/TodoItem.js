a4f113de28f907c9ff25c7e0c49e758e5c42b4dd
IFE2 <2483690020@qq.com>
2018-09-17 15:24:50 +0800

    add propTypes and defaultProps

--- todolist-by-react/src/TodoItem.js
+++ todolist-by-react/src/TodoItem.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
   constructor(props) {
     super(props)
     this.handleDelete = this.handleDelete.bind(this)
   }
-  handleDelete() {
+  handleDelete () {
     const { deleteItem, index } = this.props
     deleteItem(index)
   }
-  render() {
+  render () {
     const { content } = this.props
     return (
       <div>{content} <button onClick={this.handleDelete}>X</button></div>
     )
   }
 }
-
+// 对传入的props类型进行检测
+TodoItem.propTypes = {
+  // content必须传递
+  content: PropTypes.string.isRequired,
+  deleteItem: PropTypes.func,
+  index: PropTypes.number
+}
+// 定义props的默认值
+TodoItem.defaultProps = {
+  content: '未添加项目'
+}
 export default TodoItem
