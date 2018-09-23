27da583918fbd79a3019e598a1e55580e959c363
Jiang XiaoJun <2483690020@qq.com>
2018-09-23 18:36:28 +0800

    add styled-components and reset.css

--- jianshu/src/index.js
+++ jianshu/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// 这里引入后 全局都生效
+import './style';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
