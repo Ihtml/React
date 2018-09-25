8b127aea81f8b0b55889134516d4ddf005d6ef6f
IFE2 <2483690020@qq.com>
2018-09-25 17:59:14 +0800

    use react-redux

--- todolist-by-react/src/index.js
+++ todolist-by-react/src/index.js
@@ -1,9 +1,13 @@
-import React from 'react';
-// 把组件挂载到DOM上
-import ReactDOM from 'react-dom';
-// App组件，大写字母开头
-import Todolist from './Todolist';
-// all in js
-import './style.css'
-// 把组件挂载到Dom节点上,在JSX中，如果要使用自己创建的组件，用这种标签形式的就可以了
-ReactDOM.render(<Todolist />, document.getElementById('root'));
+import React from 'react'
+import ReactDOM from 'react-dom'
+import TodoList from './Todolist'
+import { Provider } from 'react-redux'
+import store from './store'
+
+const App = (
+  <Provider store={store}>
+    <TodoList></TodoList>
+  </Provider>
+)
+
+ReactDOM.render(App, document.getElementById('root'))
