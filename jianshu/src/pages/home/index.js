b78e2d47f8d703a7b27de407fcaf66258c219a95
Jiang XiaoJun <2483690020@qq.com>
2018-10-03 19:23:41 +0800

    get home data by axios

--- jianshu/src/pages/home/index.js
+++ jianshu/src/pages/home/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Topic from './components/Topic'
 import List from './components/List'
 import Writer from './components/Writer'
 import Recommend from './components/Recommend'
+import axios from 'axios'
 import {
   HomeWrapper,
   HomeLeft,
@@ -25,6 +27,23 @@ class Home extends Component {
       </HomeWrapper>
     )
   }
+  componentDidMount () {
+    axios.get('api/home.json').then((res) => {
+      const result = res.data.data
+      const action = {
+        type: 'change_home_data',
+        topicList:result.topicList,
+        articList: result.articList,
+        recommendList: result.recommendList
+      }
+      this.props.changeHomeData(action)
+    })
+  }
 }
 
-export default Home
+const mapDispatch = (dispatch) => ({
+  changeHomeData (action) {
+    dispatch(action)
+  }
+})
+export default connect(null, mapDispatch)(Home) 
--- jianshu/src/pages/home/store/reducer.js
+++ jianshu/src/pages/home/store/reducer.js
@@ -1,64 +1,19 @@
 import { fromJS } from 'immutable'
 
 const defaultState = fromJS({
-  topicList: [{
-    id: 1,
-    title: '@IT·互联网',
-    imgUrl: 'https://upload.jianshu.io/collections/images/14/6249340_194140034135_2.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64'
-  },
-  {
-    id: 2,
-    title: '摄影',
-    imgUrl: 'https://upload.jianshu.io/collections/images/83/1.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64'
-  }
-  ],
-  articList: [
-    {
-      id: 1,
-      title: '一段标题',
-      desc: '一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述',
-      imgUrl: 'https://upload-images.jianshu.io/upload_images/13199565-53a2ec58225fa5ec.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240'
-    },
-    {
-      id: 2,
-      title: '一段标题',
-      desc: '一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述',
-      imgUrl: 'https://upload-images.jianshu.io/upload_images/13199565-53a2ec58225fa5ec.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240'
-    },
-    {
-      id: 3,
-      title: '一段标题',
-      desc: '一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述一段很长的描述',
-      imgUrl: 'https://upload-images.jianshu.io/upload_images/13199565-53a2ec58225fa5ec.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240'
-    }
-  ],
-  recommendList: [
-    {
-      id: 1,
-      imgUrl: 'http://cdn2.jianshu.io/assets/web/banner-s-3-7123fd94750759acf7eca05b871e9d17.png'
-    },
-    {
-      id: 2,
-      imgUrl: 'https://cdn2.jianshu.io/assets/web/banner-s-4-b70da70d679593510ac93a172dfbaeaa.png'
-    },
-    {
-      id: 3,
-      imgUrl: 'https://cdn2.jianshu.io/assets/web/banner-s-7-1a0222c91694a1f38e610be4bf9669be.png'
-    },
-    {
-      id: 4,
-      imgUrl: 'https://cdn2.jianshu.io/assets/web/banner-s-5-4ba25cf5041931a0ed2062828b4064cb.png'
-    },
-    {
-      id: 5,
-      imgUrl: 'https://cdn2.jianshu.io/assets/web/banner-s-6-c4d6335bfd688f2ca1115b42b04c28a7.png'
-    }
-  ]
-
+  topicList: [],
+  articList: [],
+  recommendList: []
 })
 
 export default (state = defaultState, action) => {
   switch (action.type) {
+    case 'change_home_data':
+      return state.merge({
+        topicList: fromJS(action.topicList),
+        articList: fromJS(action.articList),
+        recommendList: fromJS(action.recommendList)
+      })
     default:
       return state
   }
