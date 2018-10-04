15e27987f7e57a317da8f45b5cf36491258beb4e
Jiang XiaoJun <2483690020@qq.com>
2018-10-04 23:40:43 +0800

    add detail styles

--- jianshu/src/pages/detail/index.js
+++ jianshu/src/pages/detail/index.js
@@ -1,9 +1,19 @@
 import React, { Component, Fragment } from 'react'
+import {
+  DetailWrapper,
+  Header,
+  Content,
+} from './style'
 
 class Detail extends Component {
   render() {
     return (
-      <Fragment>Detail </Fragment>
+      <DetailWrapper>
+        <Header>东华凤九 落叶知秋</Header>
+        <Content>
+          这是文章的内容，一段很长的文字，这是文章的内容，一段很长的文字，这是文章的内容，一段很长的文字，
+        </Content>
+      </DetailWrapper>
     )
   }
 }
