51a070c2166e4bbe0b826268aabaeb2a20d7cdc1
Jiang XiaoJun <2483690020@qq.com>
2018-10-05 20:15:55 +0800

    add Login

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './common/header'
 import Home from './pages/home'
 import Detail from './pages/detail'
+import Login from './pages/login'
 import store from './store/index'
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Fragment>
             <Header />
             <Route path='/' exact component={Home}></Route>
+            <Route path='/login' exact component={Login}></Route>
             <Route path='/detail/:id' exact component={Detail}></Route>
           </Fragment>
         </BrowserRouter>
