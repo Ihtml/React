3c81b50c08a4e8426339a7ad547b9b660d6f5b16
Jiang XiaoJun <2483690020@qq.com>
2018-10-06 21:01:01 +0800

    add Write

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,4 +1,4 @@
-import React,{ Component } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { CSSTransition } from 'react-transition-group'
@@ -21,26 +21,26 @@ import {
 } from './style'
 
 class Header extends Component {
-  render () {
+  render() {
     const {
       focused,
-      list, 
-      page, 
-      totalPage, 
-      mouseIn, 
+      list,
+      page,
+      totalPage,
+      mouseIn,
       login,
       logout,
-      handleInputFocus, 
-      handleInputBlur, 
-      handleMouseEnter, 
-      handleMouseLeave, 
+      handleInputFocus,
+      handleInputBlur,
+      handleMouseEnter,
+      handleMouseLeave,
       handleChangePage
     } = this.props
     const newList = list.toJS() //把immutable对象转化成普通对象,才能newList[i]
     const pageList = []
     // ajax获取到数据后，才做渲染
     if (newList.length) {
-      for (let i = (page -1) * 10; i < page * 10; i++) {
+      for (let i = (page - 1) * 10; i < page * 10; i++) {
         if (newList[i]) {
           pageList.push(
             <SearchInfoItem key={newList[i]}>{newList[i]}</SearchInfoItem>
@@ -48,7 +48,7 @@ class Header extends Component {
         }
       }
     }
-    
+
     return (
       <HeaderWrapper>
         <Link to="/">
@@ -59,7 +59,7 @@ class Header extends Component {
           <NavItem className='fl'>下载App</NavItem>
           {
             login ? <NavItem className='fr' onClick={logout}>退出</NavItem> :
-            <Link to="/login"><NavItem className='fr'>登录</NavItem></Link>
+              <Link to="/login"><NavItem className='fr'>登录</NavItem></Link>
           }
           <NavItem className='fr'>
             <i className="iconfont">&#xe636;</i>
@@ -70,19 +70,19 @@ class Header extends Component {
               timeout={400}
               classNames="slide"
             >
-              <NavSearch 
-              className={focused ? 'focused' : ''} 
-              onFocus={() => handleInputFocus(list)}
-              onBlur={handleInputBlur}
+              <NavSearch
+                className={focused ? 'focused' : ''}
+                onFocus={() => handleInputFocus(list)}
+                onBlur={handleInputBlur}
               ></NavSearch>
             </CSSTransition>
             <i className={focused ? 'focused iconfont zoom' : 'iconfont zoom'}>&#xe614;</i>
             <SearchInfo className={focused || mouseIn ? '' : 'dn'} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
               <SearchInfoTitle>
                 热门搜索
-                <SearchInfoSwitch onClick={() => {handleChangePage(page, totalPage, this.spinIcon)}}>
-                <i ref={(icon) => {this.spinIcon = icon}} className="iconfont spin">&#xe851;</i>
-                换一批
+                <SearchInfoSwitch onClick={() => { handleChangePage(page, totalPage, this.spinIcon) }}>
+                  <i ref={(icon) => { this.spinIcon = icon }} className="iconfont spin">&#xe851;</i>
+                  换一批
                 </SearchInfoSwitch>
               </SearchInfoTitle>
               <SearchInfoList>
@@ -91,10 +91,12 @@ class Header extends Component {
             </SearchInfo>
           </SearchWrapper>
           <Addition>
-            <Button className='writting'>
-              <i className="iconfont">&#xe615;</i>
-              写文章
+            <Link to="/write">
+              <Button className='writting'>
+                <i className="iconfont">&#xe615;</i>
+                写文章
             </Button>
+            </Link>
             <Button className='reg'>注册</Button>
           </Addition>
         </Nav>
@@ -121,23 +123,23 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleInputFocus (list) {
+    handleInputFocus(list) {
       dispatch(actionCreators.getSearchFocusAction());
       // list为空时才发送请求
       (list.size === 0) && actionCreators.getList()(dispatch)
     },
-    handleInputBlur () {
+    handleInputBlur() {
       const action = actionCreators.getSearchBlurAction()
       dispatch(action)
     },
-    handleMouseEnter () {
+    handleMouseEnter() {
       dispatch(actionCreators.mouseEnter())
     },
-    handleMouseLeave () {
+    handleMouseLeave() {
       dispatch(actionCreators.mouseLeave())
     },
-    handleChangePage (page, totalPage, spin) {
-        // 除了数字以外的字符串替换成空
+    handleChangePage(page, totalPage, spin) {
+      // 除了数字以外的字符串替换成空
       let originAngle = spin.style.transform.replace(/[^0-9]/ig, '');
       if (originAngle) {
         // 字符串以十进制转换为数字
@@ -148,15 +150,15 @@ const mapDispatchToProps = (dispatch) => {
       // 使用js原生方法改变样式
       spin.style.transform = 'rotate(' + (originAngle + 360) + 'deg)' // 字符串拼接
       if (page < totalPage) {
-        dispatch(actionCreators.changePage(page +1))
+        dispatch(actionCreators.changePage(page + 1))
       } else {
         dispatch(actionCreators.changePage(1))
       }
     },
-    logout () {
+    logout() {
       dispatch(loginActionCreators.logout())
     }
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (Header)
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
