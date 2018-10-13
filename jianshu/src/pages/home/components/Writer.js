10b138ba36ceb654e875c45d9fa14cb16b0f74f2
Jiang XiaoJun <2483690020@qq.com>
2018-10-13 15:40:50 +0800

    get writer list by axios

--- jianshu/src/pages/home/components/Writer.js
+++ jianshu/src/pages/home/components/Writer.js
@@ -5,8 +5,31 @@ import { RecoWriterTitle, WriterSwitch, WriterList, WriterItem, MoreWriter } fro
 import { actionCreators } from '../store';
 
 class Writer extends PureComponent {
-  render () {
-    const {writerPage, writerTotalPage, handleChangePage} = this.props
+  render() {
+    const { list, writerPage, writerTotalPage, handleChangePage } = this.props
+    const newList = list.toJS() //把immutable对象转化成普通对象,才能newList[i]
+    const writerList = []
+    if (newList.length) {
+      for (let i = (writerPage - 1) * 5; i < writerPage * 5; i++) {
+        if (newList[i]) {
+          writerList.push(
+            <WriterItem key={newList[i].id}>
+              <a className="avatar" target="_blank" rel="noopener noreferrer" href={newList[i].link_url}>
+                <img alt="avatar" src={newList[i].avatar_source}></img>
+              </a>
+              <Link to="/write">
+                <div className="follow">
+                  <i className="iconfont ic-follow">&#xeaf3;</i>
+                  关注
+              </div>
+              </Link>
+              <a className="name" target="_blank" rel="noopener noreferrer" href="https://www.jianshu.com/u/b6608e27dc74?utm_source=desktop&utm_medium=index-users">{newList[i].nickname}</a>
+              <div className="info">写了{newList[i].total_wordage}字 · {newList[i].total_likes_count}喜欢</div>
+            </WriterItem>
+          )
+        }
+      }
+    }
     return (
       <Fragment>
         <RecoWriterTitle>
@@ -17,30 +40,20 @@ class Writer extends PureComponent {
           </WriterSwitch>
         </RecoWriterTitle>
         <WriterList>
-          <WriterItem>
-            <a className="avatar" target="_blank" rel="noopener noreferrer" href="https://www.jianshu.com/u/b6608e27dc74?utm_source=desktop&utm_medium=index-users">
-              <img alt="avatar" src="https://upload.jianshu.io/users/upload_avatars/3884693/b425c707-d998-46a6-98b1-9b1e241513c3.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/96/h/96/format/webp"></img>
-            </a>
-            <Link to="/write">
-              <div className="follow">
-                <i className="iconfont ic-follow">&#xeaf3;</i>
-                关注
-              </div>
-            </Link>
-            <a className="name" target="_blank" rel="noopener noreferrer" href="https://www.jianshu.com/u/b6608e27dc74?utm_source=desktop&utm_medium=index-users">王诗翔</a>
-            <div className="info">写了428.6k字 · 1.6k喜欢</div>
-          </WriterItem>
+          {
+            writerList
+          }
           <MoreWriter target="_blank" rel="noopener noreferrer" href="https://www.jianshu.com/recommendations/users?utm_source=desktop&utm_medium=index-users">查看全部<i className="iconfont">&#xe6c7;</i></MoreWriter>
         </WriterList>
       </Fragment>
     )
   }
-  componentDidMount () {
+  componentDidMount() {
     this.props.getWriterList()
   }
 }
 
-const mapState = (state) => { 
+const mapState = (state) => {
   return {
     list: state.getIn(['home', 'writerList']),
     writerPage: state.getIn(['home', 'writerPage']),
@@ -49,10 +62,10 @@ const mapState = (state) => {
 }
 const mapDispatch = (dispatch) => {
   return {
-    getWriterList () {
+    getWriterList() {
       dispatch(actionCreators.getWriterList())
     },
-    handleChangePage (page, totalPage, spin) {
+    handleChangePage(page, totalPage, spin) {
       // 除了数字以外的字符串替换成空
       let originAngle = spin.style.transform.replace(/[^0-9]/ig, '')
       if (originAngle) {
@@ -69,4 +82,4 @@ const mapDispatch = (dispatch) => {
     }
   }
 }
-export default connect(mapState,mapDispatch)(Writer)
+export default connect(mapState, mapDispatch)(Writer)
